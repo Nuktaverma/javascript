d49ab627f82ed15ef56300f5a3abede3033ff62d
Nuktaverma <vermanukta@gmail.com>
2024-10-05 12:56:07 +0530

    covered datatypes and comparison

--- 01basics/03conversionOperation.js
+++ 01basics/03conversionOperation.js
@@ -1,33 +1,33 @@
-let score="33abc"
+// let score="33abc"
 
-console.log(typeof score) 
-console.log(typeof (score)) 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)  //Nan means not a number
+// console.log(typeof score) 
+// console.log(typeof (score)) 
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)  //Nan means not a number
 
-let score1=null
-console.log(typeof score1) 
-console.log(typeof (score1)) 
-let valueInNumber1=Number(score1)
-console.log(typeof valueInNumber1)
-console.log(valueInNumber1)  
+// let score1=null
+// console.log(typeof score1) 
+// console.log(typeof (score1)) 
+// let valueInNumber1=Number(score1)
+// console.log(typeof valueInNumber1)
+// console.log(valueInNumber1)  
 
-let score2=null
-console.log(typeof score2) 
-console.log(typeof (score2)) 
-let valueInNumber2=Number(score2)
-console.log(typeof valueInNumber2)
-console.log(valueInNumber2)  
+// let score2=null
+// console.log(typeof score2) 
+// console.log(typeof (score2)) 
+// let valueInNumber2=Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)  
 
 
 // "33" =>33
 // "33abc" => Nan
 // true => 1 ; false => 0
 
-let isloggedIn=1
-let booleanIsloggedIn=Boolean(isloggedIn)
-console.log(booleanIsloggedIn)
+// let isloggedIn=1
+// let booleanIsloggedIn=Boolean(isloggedIn)
+// console.log(booleanIsloggedIn)
 
 
 //1=>true
@@ -35,7 +35,49 @@ console.log(booleanIsloggedIn)
 //""=>false
 //"nukta"=> true
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// *****************************operations ************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 3);
+console.log(2 / 3);
+console.log(2 % 3);
+
+
+let str1 = "hello"
+let str2 = " Nukta"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);   //print 12
+console.log("1" + "2")    //print 12
+
+console.log("1" + 2 + 2)   //print 122
+console.log(1 + 2 + "2");   //print 32
+
+console.log(+true)  //1
+console.log(+"")  //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+
+++gameCounter
+console.log(gameCounter)
+
