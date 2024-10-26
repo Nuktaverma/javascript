117d253dd3757e82e712444492adef83711b1f30
Nuktaverma <vermanukta@gmail.com>
2024-10-26 21:52:05 +0530

    started with iterations

--- 04control_flow/truthy.js
+++ 04control_flow/truthy.js
@@ -35,3 +35,18 @@ const emptyObj={}
 
 
 // Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1=5??10           //5
+// val1=null??10            //10
+// val1=undefined??15          //15
+//val1=null??10??20         //10
+
+//console.log(val1);
+ 
+
+// Terniary Operator
+//condition ? true: false
+
+const iceTeaPrice=100
+iceTeaPrice>=80 ? console.log("Less than 80") : console.log("Greater than 80");
